6664e249324e54b06ab6845164169a49e1ff81e5
Kaleem <kaleem.peeroo@city.ac.uk>
2018-06-25 16:32:14 +0100

    Refactored card creation for exercise input

--- add_workout.php
+++ add_workout.php
@@ -39,46 +39,90 @@
 
 		<script>
 			function addExercise(){
-				/*
-					Overview of elements and variable names:
-					LINode: LI element to be added to the UL element
-					anchorNode: <p></p> element to be inserted into the li element
-					text: text to be inserted into the paragraph tag
-				*/
-				//Create the LI element which will be empty
-				var LINode = createElement("div", "card");
-				//Set the class of the LI element created
-				LINode.className += " exercise_input";
-				//Create the paragraph tag that will contain the text within it
-				var anchorNode = document.createElement("p", "");
-				//Create the text that will go into the paragraph tag
-				var text = document.createTextNode(document.getElementById("exerciseName").value);
-				//Create and configure input element
-				var rep_input = createElement("input", "");
-				rep_input.setAttribute("type", "number");
-				rep_input.setAttribute("min", "1");
-				rep_input.setAttribute("max", "1000");
-				rep_input.className += "rep_input";
-
-				//Create div
-				var input_div = createElement("div", "ui input");
-				input_div.appendChild(rep_input);
-
-				//Append the text to the paragraph tag making the text a link
-				anchorNode.appendChild(text);
-				//Append the text in the paragraph tag to the li element 
-				LINode.appendChild(anchorNode);
-				LINode.appendChild(input_div);
-				//Append the li element with the link in it to the ul list
-				document.getElementById("exerciseList").appendChild(LINode);
-				//Clear the input after exercise is added to the list
+				var card = createCard();
+				document.getElementById("exerciseList").appendChild(card);
 				document.getElementById("exerciseName").value = "";
+			}
+
+			function createCard(){
+				/*
+					<div class="card"></div>
+				*/
+				var card = createElement("div", "card");
+				/*
+					<div class="content"></div>
+				*/
+				var content = createElement("div", "content");
+				/*
+					<p class="header"></p>
+				*/
+				var header = createElement("p", "header");
+				/*
+					<p class="header">input value</p>
+				*/
+				var header_text = document.createTextNode(document.getElementById("exerciseName").value);
+				/*
+					<div class="ui right labeled input"></div>
+				*/
+				var input_div = createElement("div", "ui right labeled input");
+				/*
+					<input type="text" class="" placeholder="Enter rep amount"/>
+				*/
+				var input = createInput("text", "", "Enter rep amount");
+				/*
+					<div class="ui basic label"></div>
+				*/
+				var label_div = createElement("div", "ui basic label");
+				/*
+					<div class="ui basic label">reps</div>
+				*/
+				var label_div_text = document.createTextNode("reps");
+
+				/*
+					<div class="ui basic label">reps</div>
+				*/
+				label_div.appendChild(label_div_text);
+				/*
+					<div class="ui right labeled input">
+						<input type="text" class=" "" placeholder="Enter rep amount"/>
+					</div>
+				*/
+				input_div.appendChild(input);
+				/*
+					<div class="ui right labeled input">
+						<input type="text" class=" " placeholder="Enter rep amount"/>
+						<div class="ui basic label">reps</div>
+					</div>
+				*/
+				input_div.appendChild(label_div);
+				/*
+					
+				*/
+				header.appendChild(header_text);
+				content.appendChild(header);
+				content.appendChild(input_div);
+				card.appendChild(content);
+
+				return card;
 			}
 
 			function createElement(elementName, className){
 				element = document.createElement(elementName);
 				element.className += " " + className;
 				return element;	
+			}
+
+			function createInput(type, class_name, placeholder_value){
+				var input = createElement("input", " " + class_name);
+				input.setAttribute("type", type);
+				input.setAttribute("placeholder", placeholder_value);
+
+				return input;
+			}
+
+			function setCardContent(){
+				var contentDiv = createElement("div", "content");
+				return contentDiv;
 			}
 
 		</script>
