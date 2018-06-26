ae3e79b19458dd7fb754c37d8cdaddb5f90070d7
Kaleem <kaleem.peeroo@city.ac.uk>
2018-06-26 20:29:52 +0100

    Added event listener so now user can press enter to insert exercise card

--- add_workout.php
+++ add_workout.php
@@ -5,40 +5,48 @@
 		<link rel="stylesheet" href="css/add_workout_style.css">
 		<?php require_once('dependencies.php'); ?>
 	</head>
-	<body>
+	<body onload="prefunction()">
 		<div class="ui container">
-			<form action="">
-				<p>
-					<h1 class="ui header">Add A Workout</h1>
-				</p>
-				<p>
-					<div class="ui input">
-						<input type="text" placeholder="Enter Workout Title">
-					</div>
-				</p>
-				<p>
-					<div class="ui input">
-						<input type="text" id="exerciseName" placeholder="Enter exercise name here">
-					</div>
-					<span onclick="addExercise()" class="ui primary basic button">
-						Add Exercise
-					</span>
-				</p>
-				<!-- <ul id="exerciseList">
-				</ul> -->
-				<div class="ui link cards" id="exerciseList">
-					
+			<p>
+				<h1 class="ui header">Add A Workout</h1>
+			</p>
+			<p>
+				<div class="ui input">
+					<input type="text" placeholder="Enter Workout Title">
 				</div>
-				<div class="ui buttons add_exercise_button">
-					<a href="index.php" class="ui button">Cancel</a>
-					<div class="or"></div>
-					<input type="submit" value="Save Workout" class="ui positive button"></input>
+			</p>
+			<p>
+				<div class="ui input">
+					<input type="text" id="exerciseName" placeholder="Enter exercise name here">
 				</div>
-			</form>
+				<span onclick="addExercise()" class="ui primary basic button">
+					Add Exercise
+				</span>
+			</p>
+			<!-- <ul id="exerciseList">
+			</ul> -->
+			<div class="ui link cards" id="exerciseList">
+				
+			</div>
+			<div class="ui buttons add_exercise_button">
+				<a href="index.php" class="ui button">Cancel</a>
+				<div class="or"></div>
+				<input type="submit" value="Save Workout" class="ui positive button"></input>
+			</div>
 		</div>
 	
 		<script src="js/global_functions.js"></script>
 		<script>
+			function prefunction(){
+				var input = document.getElementById("exerciseName");
+
+				input.addEventListener("keydown", function(e){
+					if (e.keyCode === 13) {
+						addExercise();
+					}
+				});
+			}
+
 			function addExercise(){
 				var card = createExerciseCard();
 				document.getElementById("exerciseList").appendChild(card);
