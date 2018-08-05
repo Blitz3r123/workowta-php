c7964dbb7cf4f3263645beb6b29e5d192cd357f7
Kaleem <kaleem.peeroo@city.ac.uk>
2018-08-05 19:05:03 +0100

    Added login page and sign out button

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Workowta Login</title>
+		<link rel="stylesheet" href="css/login_style.css">
+		<link rel="stylesheet" href="css/global_styles.css">
+		<?php require_once('dependencies.php'); ?>
+	</head>
+	<body style="background-color: #2185d0;">
+		<div class="ui container margin-top-10vh">
+			<h1 class="ui header" style="width: 50%; margin-left: 25%; text-align: center; color: white;">Workowta</h1>
+			<div class="ui margin-top-20vh" style="width: 50%; margin-left: 25%;">
+				<div class="row centered-width-50 text-align-center">
+					<div class="ui left icon input">
+						<input placeholder="Enter Username" type="text">
+						<i class="users icon"></i>
+					</div>
+				</div>
+				<div class="row centered-width-50 text-align-center margin-top-1vh">
+					<div class="ui left icon input">
+						<input placeholder="Enter Password" type="text">
+						<i class="lock icon"></i>
+					</div>
+				</div>
+				<div class="row centered-width-50 text-align-center">
+					<div class="ui label margin-top-1vh">
+						<a href="index.php" class="ui labeled button">
+							Login
+							<i class="icon sign in alternate" style="margin-left: 0.3vw;"></i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script
+		src="https://code.jquery.com/jquery-3.3.1.js"
+		integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+		crossorigin="anonymous"></script>
+		<script src="js/global_functions.js"></script>
+	</body>
+</html>
--- right_menu.php
+++ right_menu.php
@@ -1,4 +1,13 @@
 <div class="right menu">
+	<div class="ui button negative basic">
+		<a 
+			href="login.php"
+			class="ui labeled" 
+		   	style="line-height: 3vh; text-align: center; color: #db2828;">
+				Sign Out
+				<i class="icon sign out alternate" style="margin-left: 0.4vw;"></i>
+		</a>
+	</div>
 	<a class="ui vertical animated fade button" tabindex="0" id="createWorkoutButton" onclick="showWorkoutType()">
 		<div class="visible content">
 			Create...
